58db3ae1d4c7c94789925858f8fd6da2367f2d92
Brandon Kaminski <140101047+bkaminski126@users.noreply.github.com>
2024-01-18 12:03:29 -0500

    about fadein

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -9,10 +9,10 @@ export default function About(props) {
 
   return (
     <div className="z-20 pb-8 mt-2 about pt-14 md:pt-0">
-      <h1 className="my-2 text-xl font-bold md:mt-4">
+      <h1 className="aboutitem my-2 text-xl font-bold md:mt-4 animate-[fadein_1s_ease-in-out_0s]">
         {data[`${lang}`].about_page.fast_facts_title}
       </h1>
-      <div className="grid justify-between grid-cols-1 gap-2 p-2 text-sm text-black bg-yellow-600 rounded-xl md:grid-cols-2">
+      <div className="aboutitem grid justify-between grid-cols-1 gap-2 p-2 text-sm text-black bg-yellow-600 rounded-xl md:grid-cols-2  animate-[fadein_1s_ease-in-out_0.1s]">
         {facts.map((f) => {
           let fs = f.split(":");
           return (
@@ -26,15 +26,17 @@ export default function About(props) {
       </div>
 
       {/* bio */}
-      <h1 className="my-2 text-xl font-bold">
+      <h1 className="aboutitem my-2 text-xl font-bold  animate-[fadein_1s_ease-in-out_0.2s]">
         {data[`${lang}`].about_page.bio_title}
       </h1>
 
-      {texts.map((t) => (
-        <p key={t} className="my-2">
-          {t}
-        </p>
-      ))}
+      <div className="aboutitem animate-[fadein_1s_ease-in-out_0.3s]">
+        {texts.map((t) => (
+          <p key={t} className="my-2">
+            {t}
+          </p>
+        ))}
+      </div>
     </div>
   );
 }
