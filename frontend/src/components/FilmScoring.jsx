14dbe35333b9c3d3c7ce3d40523744b2860b9d09
Brandon Kaminski <140101047+bkaminski126@users.noreply.github.com>
2024-05-01 14:50:34 -0400

    film scoring

--- frontend/src/components/FilmScoring.jsx
+++ frontend/src/components/FilmScoring.jsx
@@ -1,21 +1,16 @@
-import { faYoutube } from "@fortawesome/free-brands-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
 import data from "../data/translate.json";
-import Countdown from "./Countdown";
-import ParseISO from "../data/ParseISO";
 
 export default function FilmScoring(props) {
   const { lang } = props;
   return (
-    <div className="z-20 pb-8 mt-2 about pt-14 md:pt-0">
+    <div className="z-20 pb-16 mt-2 about pt-14 md:pt-0">
       <h1 className="my-2 text-xl font-bold md:mt-4">
         {data[`${lang}`].film_page.title}
       </h1>
       <p className="mb-8 text-lg">{data[`${lang}`].film_page.text}</p>
       <div className="flex flex-col gap-8">
         <h2 className="text-xl font-bold text-center md:text-2xl">
-        {data[`${lang}`].film_page.view}
+          {data[`${lang}`].film_page.view}
         </h2>
         <div className="flex flex-col gap-8 md:justify-center md:items-center md:flex-row">
           <div className="flex flex-col gap-2 md:w-1/4">
@@ -24,12 +19,14 @@ export default function FilmScoring(props) {
             </span>
             <div className="flex flex-row flex-wrap items-end md:text-right md:gap-2 md:flex-col">
               <span className="text-lg grow">Dir. Jack Gentry</span>
-              <span className="text-lg grow">{data[`${lang}`].film_page.genre_1}</span>
+              <span className="text-lg grow">
+                {data[`${lang}`].film_page.genre_1}
+              </span>
               <span className="text-lg grow">2023</span>
             </div>
           </div>
           <iframe
-            className="aspect-video md:w-3/5"
+            className="aspect-video md:w-3/5 md:max-w-[850px]"
             src="https://www.youtube.com/embed/oxBLW7-3Hh4?si=axUSssOjHi_LF4zZ"
             title="Glass Canvas video player"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
@@ -44,12 +41,14 @@ export default function FilmScoring(props) {
             </span>
             <div className="flex flex-row flex-wrap items-end md:text-right md:gap-2 md:flex-col">
               <span className="text-lg grow">Dir. Kolby Oglesby</span>
-              <span className="text-lg grow">{data[`${lang}`].film_page.genre_2}</span>
+              <span className="text-lg grow">
+                {data[`${lang}`].film_page.genre_2}
+              </span>
               <span className="text-lg grow">2022</span>
             </div>
           </div>
           <iframe
-            className="aspect-video md:w-3/5"
+            className="aspect-video md:w-3/5 md:max-w-[850px]"
             src="https://www.youtube.com/embed/Lu3-j0PGOV8?si=yCInFNNWL3DZFl4W"
             title="Glass Canvas video player"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
@@ -64,12 +63,14 @@ export default function FilmScoring(props) {
             </span>
             <div className="flex flex-row flex-wrap items-end md:text-right md:gap-2 md:flex-col">
               <span className="text-lg grow">Dir. Bailey Johnson</span>
-              <span className="text-lg grow">{data[`${lang}`].film_page.genre_3}</span>
+              <span className="text-lg grow">
+                {data[`${lang}`].film_page.genre_3}
+              </span>
               <span className="text-lg grow">2022</span>
             </div>
           </div>
           <iframe
-            className="aspect-video md:w-3/5"
+            className="aspect-video md:w-3/5 md:max-w-[850px]"
             src="https://www.youtube.com/embed/jEZ-Cu9aNpU?si=7cMoCWEK4h5Gugyq"
             title="Glass Canvas video player"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
