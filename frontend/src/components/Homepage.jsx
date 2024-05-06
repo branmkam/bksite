efb2b2a13c8e3bcce22791d3b412b04e22f3c74c
Brandon Kaminski <140101047+bkaminski126@users.noreply.github.com>
2024-05-06 13:55:02 -0400

    add new countdown

--- frontend/src/components/Homepage.jsx
+++ frontend/src/components/Homepage.jsx
@@ -10,7 +10,6 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import data from "../data/translate.json";
 import Countdown from "./Countdown";
-import ParseISO from "../data/ParseISO";
 
 export default function Homepage(props) {
   const { lang } = props;
@@ -22,33 +21,38 @@ export default function Homepage(props) {
         "h-[95vh] z-50 mt-2 flex flex-col text-center justify-center items-center animate-[fadein_1s_ease-in-out]"
       }
     >
-      <h1 className="z-10 text-lg text-orange-400 transition md:text-2xl font-sigmar">
-        <a
+      <h1 className="z-10 text-lg transition text-slate-100 md:text-2xl font-sigmar">
+        {/* <a
           className="underline cursor-pointer hover:text-orange-300"
           href="https://distrokid.com/hyperfollow/brankam/charlottean-feat-aidan-cundiff-2?utm_campaign=website&utm_medium=Email+&utm_source=SendGrid"
           rel="noreferrer"
           target="_blank"
-        >
-          {data[`${lang}`].homepage.new_music.toLowerCase()}
-        </a>
+        > */}
+        {data[`${lang}`].homepage.text.toLowerCase()}
+        {/* </a> */}
       </h1>
+      <h2 className="m-1 text-lg text-orange-400 font-sigmar">
+        {data[`${lang}`].homepage.reveal.toLowerCase()}
+      </h2>
 
-      <div className="m-2 text-center md:m-6 h-1/2">
+      <div className="relative flex flex-col m-2 text-center md:m-6 h-1/2">
         <a
           className="underline cursor-pointer hover:text-orange-300"
           href="https://distrokid.com/hyperfollow/brankam/charlottean-feat-aidan-cundiff-2?utm_campaign=website&utm_medium=Email+&utm_source=SendGrid"
           rel="noreferrer"
           target="_blank"
-        >
-          <img
-            className="hover:brightness-125 z-0 object-contain h-full brightness-[100%]"
-            src="./charlotteangif.gif"
-          />
-        </a>
+        ></a>
+
+        <img
+          className="hover:brightness-125 z-0 object-contain h-full brightness-[60%]"
+          src="./globespin-transparent.gif"
+        />
+        <Countdown
+          className="absolute flex items-center justify-center w-full h-full text-4xl font-bold text-orange-400 drop-shadow-[8px_35px_10px_rgba(0,0,0,0.1)] align-center md:text-6xl lg:text-8xl"
+          time={"2024-05-11T00:00:00"}
+          seconds
+        />
       </div>
-
-      {/* <img src="globespin-transparent.gif" className="z-0 w-1/2 mb-2 h-1/2" /> */}
-
       <span className="z-50 text-2xl md:text-4xl">
         <a
           className="m-2 transition-colors duration-200 ease-in-out hover:text-yellow-500 hover"
