17cd2f1433601e0669094c577cc02ea2727d5868
Brandon Kaminski <140101047+bkaminski126@users.noreply.github.com>
2024-05-13 11:13:55 -0400

    homepage

--- frontend/src/components/Homepage.jsx
+++ frontend/src/components/Homepage.jsx
@@ -31,25 +31,31 @@ export default function Homepage(props) {
         {data[`${lang}`].homepage.text.toLowerCase()}
         {/* </a> */}
       </h1>
-      <h2 className="m-1 text-sm text-orange-400 sm:text-base md:text-lg font-sigmar">
-        {data[`${lang}`].homepage.reveal.toLowerCase()}
+      <h2 className="m-1 text-lg text-orange-400 underline hover:text-orange-200 sm:text-xl md:text-2xl font-sigmar">
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://distrokid.com/hyperfollow/brankam/troppo"
+        >
+          Presave
+        </a>
       </h2>
 
       <div className="relative flex flex-col m-2 text-center md:m-6 h-1/2">
         <a
-          className="underline cursor-pointer hover:text-orange-300"
-          href="https://distrokid.com/hyperfollow/brankam/charlottean-feat-aidan-cundiff-2?utm_campaign=website&utm_medium=Email+&utm_source=SendGrid"
+          className="h-full underline cursor-pointer hover:text-orange-300"
+          href="https://distrokid.com/hyperfollow/brankam/troppo"
           rel="noreferrer"
           target="_blank"
-        ></a>
-
-        <img
-          className="hover:brightness-125 z-0 object-contain h-full brightness-[60%]"
-          src="./globespin-transparent.gif"
-        />
+        >
+          <img
+            className="hover:brightness-125 z-0 object-contain h-full brightness-[60%]"
+            src="https://distrokid.imgix.net/http%3A%2F%2Fgather.fandalism.com%2F2209533--EDA0FC44-AF5A-4D2F-BF8CF432ABD7E5F8--0--3367815--troppofinal.png?fm=jpg&q=75&w=800&s=31ab52b5e045f9e69e9be1a14f91b3cc"
+          />
+        </a>
         <Countdown
           className="absolute flex items-center justify-center w-full h-full text-4xl font-bold text-orange-400 drop-shadow-[8px_35px_10px_rgba(0,0,0,0.1)] align-center md:text-6xl lg:text-8xl"
-          time={"2024-05-11T00:00:00"}
+          time={"2024-05-24T00:00:00"}
           seconds
         />
       </div>
