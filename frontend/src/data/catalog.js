8ea40b876317905b4b159764b324253b7cade065
Brandon Kaminski <54331071+branmkam@users.noreply.github.com>
2024-05-24 00:42:38 -0400

    readd counterfeit

--- frontend/src/data/catalog.js
+++ frontend/src/data/catalog.js
@@ -16,6 +16,24 @@ const catalog = [
     },
   },
   {
+    name: "COUNTERFEIT (linguafranca remix)",
+    type: "single",
+    release_date: "2023-11-06T00:00:00",
+    image:
+      "https://is1-ssl.mzstatic.com/image/thumb/Music126/v4/97/11/2d/97112d87-0955-f189-98db-161cabcf2cdc/artwork.jpg/316x316bb.webp",
+    collab_artists: [
+      { name: "Yak Mishra", link: "https://www.youtube.com/c/Yaktube" },
+    ],
+    platforms: {
+      spotify:
+        "https://open.spotify.com/track/6iQL6qYiBzFbAXZPbL9TZ8?autoplay=true",
+      youtube:
+        "https://www.youtube.com/playlist?list=OLAK5uy_k2UG2pKyUbijW0hEtRMg2nzOWQ1mkSn-E",
+      apple:
+        "https://music.apple.com/us/album/counterfeit-linguafranca-remix-single/1714115027",
+    },
+  },
+  {
     name: "Expectations",
     type: "single",
     release_date: "2023-02-10T00:00:00",
